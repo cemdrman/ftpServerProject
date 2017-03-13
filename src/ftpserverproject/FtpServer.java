a94b45187f0019ed1f3bb22eaa0f0bb1950a60e8
Cem Dırman <cemdrman@gmail.com>
2017-03-13 18:55:32 +0300

    added makeMainDirectory method

--- src/ftpserverproject/FtpServer.java
+++ src/ftpserverproject/FtpServer.java
@@ -1,5 +1,6 @@
 package ftpserverproject;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -18,28 +19,25 @@ public class FtpServer {
     private final int serverPort = 1907;
     private InetAddress address;
     private Socket socket;
+    private File file;
+    private String systemPath = System.getProperty("user.home");
+    private final String serverDirectoryPath = systemPath + "\\desktop\\FtpServerDirectory"; //Directory masaüstüne açılması için her pcye uygun şekilde path alıyoruz
 
     public void startServer() {
-        init();
-        System.out.println("---------FTP-SERVER---------");
-        System.out.println("****************************");
-        System.out.println("Server Info");
-        System.out.println("Server IP         : " + address.getHostName());
-        System.out.println("Server Port Number: " + String.valueOf(serverPort));
-        System.out.println("****************************");
-        System.out.println("Server Started...");
-        System.out.println("Waiting for connections...");
-        
-
-    }
-    public int getServerPort(){
-        return serverPort;
-    }
-    
-    private void init() {
-        try {
+        try {            
+            file = new File(serverDirectoryPath);            
             address = InetAddress.getByName(serverIp);
             serverSocket = new ServerSocket(serverPort);
+            System.out.println("---------FTP-SERVER---------");
+            System.out.println("****************************");
+            System.out.println("Server Info");
+            System.out.println("Server directory path:" + serverDirectoryPath);
+            System.out.println("Server IP            : " + address.getHostName());
+            System.out.println("Server Port Number   : " + String.valueOf(serverPort));
+            System.out.println("****************************");
+            System.out.println("Server Started...");
+            System.out.println("Waiting for connections...");
+            makeMainDirectory();//öncelikle ana Directoryi oluşturuyoruz
             socket = serverSocket.accept();//client bağlanana kadar burda bekler
             System.out.println("New Client Connected from " + socket.getInetAddress().getHostName() + "...");
             ServerThread serverThread = new ServerThread(socket);
@@ -48,6 +46,23 @@ public class FtpServer {
             Logger.getLogger(FtpServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
+
+    private void makeMainDirectory() {
+        if (file.exists()) { //böyle bir klasör var mı?
+            System.out.println("Directory is allready created!");
+        } else if (file.mkdir()) {
+            System.out.println("Directory is created!");
+        } else {
+            System.out.println("Directory is not created!");
+        }
+    }
+
+    private void allFileListOnTheServer() {
+        file.listFiles();//ana dir altındaki bütün dosyalar
+    }
+
+    public int getServerPort() {
+        return serverPort;
+    }
+
 }
-
-
