bb6ea032756e396124c1797ea40aa90eb258e415
Cem Dırman <cemdrman@gmail.com>
2017-05-08 19:30:29 +0300

    deleted FrameAnasayfa

--- src/clientPackage/Client.java
+++ src/clientPackage/Client.java
@@ -15,76 +15,77 @@ import java.net.Socket;
  */
 public class Client {
 
-    private String userName;
-    private int password;
+    private String name;
+    private String surname;
     private Socket serverSocket;
     private ClientThread clientThread;
-    private ObjectOutputStream outputStream;
-    protected int status = 0;
-    private boolean isConnected = false;
-    
-    protected void connetServer() {
-        clientThread = new ClientThread();
-        clientThread.start();
+    private ObjectOutputStream outputStream; 
+      
+    protected void connetServer(String nameSurname) {
+       
         try {
-            serverSocket = new Socket("localhost", 1907);
+            serverSocket = new Socket("localhost", 1907);  
             outputStream = new ObjectOutputStream(serverSocket.getOutputStream());
-            sendMessage("emir asaf");
-            System.out.println("Connection accepted " + serverSocket.getInetAddress() + "/" + serverSocket.getPort());           
-            
+            sendMessage(nameSurname);
+            System.out.println("Connection accepted " + serverSocket.getInetAddress() + "/" + serverSocket.getPort());
+
         } catch (IOException ex) {
             Logger.getLogger(FrameGiris.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    protected String getUserName() {
-        return userName;
+    protected String getName() {
+        return name;
     }
 
-    protected void setUserName(String userName) {
-        this.userName = userName;
+    protected void setName(String userName) {
+        this.name = userName;
     }
 
-    protected int getPassword() {
-        return password;
+    protected String getSurname() {
+        return surname;
     }
 
-    protected void setPassword(int password) {
-        this.password = password;
+    protected void setSurname(String surname) {
+        this.surname = surname;
     }
 
-    protected void sendMessage(String message) throws IOException {
+    protected void sendMessage(String message) throws IOException {       
         outputStream.writeObject(message);
+
     }
+    
+    
 
     /**
-     * 
-     * @param file firstly, the name of file has been sending for server side file name
-     * @throws IOException 
+     *
+     * @param file firstly, the name of file has been sending for server side
+     * file name
+     * @throws IOException
      */
     protected void uploadFileToServer(File file) throws IOException {
         sendMessage("saveFile"); //yapacağımız işlemi servera bildiriyoruz
-        sendMessage(file.getName());              
+        sendMessage(file.getName());
         byte[] bytes = new byte[(int) file.length()];
-       
+
         FileInputStream is = new FileInputStream(file);
         OutputStream out = serverSocket.getOutputStream();
         int count;
-        while ((count = is.read(bytes)) > 0) {            
+        while ((count = is.read(bytes)) > 0) {
             out.write(bytes, 0, count);
             break;
         }
-        
-        System.out.println("Sending " + file.getName() + "(" + bytes.length + " bytes)");      
+
+        System.out.println("Sending " + file.getName() + "(" + bytes.length + " bytes)");
         System.out.println("Done.");
-        is.close();
+
     }
 
     class ClientThread extends Thread {
 
         @Override
         public void run() {
-                      
+
         }
     }
 }
